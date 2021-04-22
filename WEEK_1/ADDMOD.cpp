9b19010c44bea0906cbc6638c13658fb66ef9469
hoangIT1 <55548345+hoangIT1@users.noreply.github.com>
2021-04-23 00:07:21 +0700

    Create ADDMOD.cpp

---
+++ WEEK_1/ADDMOD.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <cmath>
+using namespace std;
+ 
+const long long x = 1000000007;
+int main()
+{
+    unsigned long long a, b, c;
+    cin >> a >> b;
+    c = a + b;
+    cout << c % x;
+}
